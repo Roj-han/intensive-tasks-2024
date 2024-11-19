342864c186f139553f0f469820f31a997a7ef760
Roj-han <c.nastya2012@gmail.com>
2024-11-19 23:50:35 +0600

    task1: - условия, возвращающие строку "лет", объединены в одно, согласно рекомендациям ревью. Но разве много склеенных условий не ухудшают читаемость?

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -24,17 +24,14 @@ public class Task1 {
         if (age < 0 || age > 127) {
             return "Некорректный ввод";
         }
-        if (age % 100 > 10 && age % 100 < 20) {
+
+        if ((age % 100 > 10 && age % 100 < 20) || age % 10 == 0 || age % 10 > 4) {
             return "Вам " + age + " лет";
         }
+
         if (age % 10 == 1) {
             return "Вам " + age + " год";
         }
-        if (age % 10 == 0 || age % 10 > 4) {
-            return "Вам " + age + " лет";
-        }
-
         return "Вам " + age + " года";
-
     }
 }
