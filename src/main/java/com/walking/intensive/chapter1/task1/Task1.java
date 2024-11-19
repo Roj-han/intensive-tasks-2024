4d1fc6ecb8a130bb53743abcee656f6fcebff1fd
Roj-han <c.nastya2012@gmail.com>
2024-11-19 23:24:25 +0600

    task1: - убрана переменная String result; возврат строки через return в каждом условии - убран else if после return в предыдущих if (до этого там была переменная, которая возвращалась в конце метода) - все однострочные if оформлены в {}

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,7 +15,7 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 19775;
+        int age = 49;
 
         System.out.println(getAgeString(age));
     }
@@ -24,16 +24,17 @@ public class Task1 {
         if (age < 0 || age > 127) {
             return "Некорректный ввод";
         }
-        String result = "Вам " + age;
-
         if (age % 100 > 10 && age % 100 < 20) {
-            return result + " лет";
-        } else if (age % 10 == 1) {
-            return result + " год";
-        } else if (age % 10 == 0 || age % 10 > 4) {
-            return result + " лет";
-        } else {
-            return result + " года";
+            return "Вам " + age + " лет";
         }
+        if (age % 10 == 1) {
+            return "Вам " + age + " год";
+        }
+        if (age % 10 == 0 || age % 10 > 4) {
+            return "Вам " + age + " лет";
+        }
+
+        return "Вам " + age + " года";
+
     }
 }
